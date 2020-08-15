28cfe499413b948ecf06f03d3fbf020ce1431474
Jo Phillips <joxphillips@gmail.com>
2020-08-14 19:43:25 -0600

    Parse HTML h1 Title & Enclose With Tags

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-use std::fs::File;
 //use std::io::BufRead;
 //use std::io::BufReader;
-use std::path::PathBuf;
-use clap::{Arg, App};
+//use std::path::PathBuf;
+//
+use clap:: App;
+use std::fs;
+
+extern crate nom;
+use nom::{
+    sequence::delimited,
+    IResult,
+    bytes::complete::tag,
+    bytes::complete::is_not,
+
+};
 
 fn main() {
     let matches = App::new("rss_gen")
@@ -12,17 +22,33 @@ fn main() {
     .arg("-i --input=[file] 'blog post html file'")
     .arg("-o --output=[file] 'sets the file to put blog post xml data if one is not supplied a file will be created")
     .get_matches();
-    let file_name = String::new();
 
-    match matches.value_of('i') {
+    let mut file_name = String::new();
+    //println!("{:?}",matches.value_of("input"));
+    match matches.value_of("input") {
         Some(x) => file_name = x.to_string(),
         None => println!("Please enter a file to parse"),
     }
-    let f = File::open(file_name); 
+    let mut contents = fs::read_to_string(file_name)
+        .expect("Something went wrong opening the file.");
+    let title = match parse_title(contents.as_mut_str()) {
+        Ok(i) => i,
+            
+        Err(e) => 
+            panic!("parse blogpost title failed {:?}",e),
+    };
+    let _body = title.0;
+    let mut header = title.1.to_string();
+    let header = enclose(header.as_mut_str(),"title");
+    
+    println!("{:?}", header );
 }
 
+fn parse_title(input: &str) -> IResult<&str, &str> {
+    delimited(tag("<h1>"), is_not("<"), tag("</h"))(input)
+}
 
-fn enclose_tags( content: &str, tag_type: &str) -> String {
+fn enclose(content: &str, tag_type: &str) -> String {
     let mut tag= String::from("<");
     let end_tag_start="</";
     let tag_end=">";
@@ -32,17 +58,27 @@ fn enclose_tags( content: &str, tag_type: &str) -> String {
     tag.push_str(end_tag_start);
     tag.push_str(tag_type);
     tag.push_str(tag_end);
+
     tag
 }
-
+#[cfg(test)]
+mod tests {
+use super::*;
 #[test]
 fn title_string() {
-    let test_title = enclose_tags("title","Description");
+    let test_title = enclose("title","Description");
     assert_eq!(test_title, "<Description>title</Description>".to_string());
-}
+    }
 #[test]
 fn body_string() {
-    let body = enclose_tags("some blog", "title");
+    let body = enclose("some blog", "title");
 
     assert_eq!(body, "<title>some blog</title>");
+    }
+#[test]
+fn title_parse() {
+    let input_str = "<h1>header</h1>";
+    let parse_title = parse_title(input_str);
+    assert_eq!(parse_title, Ok(("1>","header")));
+}
 }
