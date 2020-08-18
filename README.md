d05c5170c5ce570cf03e1bd999c5e8adee908132
Jo Phillips <joxphillips@gmail.com>
2020-08-17 22:30:10 -0600

    Parse Title | Parse body
    currently only works with my style of blog entry with php.
    needs further fault tolerance and testing to make sure it correctly
    parses titles and bodys. right now it will look for the first h2 tag and
    parse the inner text as the title. It will spit out the rest into the
    body. the body will need further cleaning.

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 commandline rss feed generator
 
-TODO: read in text from file. 
-parse data til the h tag is reached.
-stop parsing when the next h tag is found.
+reads in html or php spits out a rss entry in a file you supply or one created with a config file.
+
+TODO: read in text from file.✓
+parse data til the h tag is reached. ✓
+stop parsing when the next h tag is found.✓
 parse the body including pictures and save pictures file paths in data.
 create a post object that will store the post data before outputing it to the xml file.
 attach post struct data with appropriate tags
 insert data into xml file.
+
+
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@
 //
 use clap:: App;
 use std::fs;
-
 extern crate nom;
 use nom::{
     sequence::delimited,
     IResult,
     bytes::complete::tag,
     bytes::complete::is_not,
+    bytes::complete::take_until,
+    branch::Alt,
+    bytes::complete::take,
 
 };
 
@@ -21,6 +23,7 @@ fn main() {
     .about("Reads in a html file and creates a rss entry for a blog.")
     .arg("-i --input=[file] 'blog post html file'")
     .arg("-o --output=[file] 'sets the file to put blog post xml data if one is not supplied a file will be created")
+    .arg("-c --config=[file] 'sets a config file to use for the url and other information that can't be gleemed from the input file.'")
     .get_matches();
 
     let mut file_name = String::new();
@@ -31,21 +34,45 @@ fn main() {
     }
     let mut contents = fs::read_to_string(file_name)
         .expect("Something went wrong opening the file.");
-    let title = match parse_title(contents.as_mut_str()) {
+    let header_garbage = match strip_till_title(contents.as_mut_str()) { // should return tuple of .0=header_garbage .1 rest of the contents of the file.
         Ok(i) => i,
             
         Err(e) => 
-            panic!("parse blogpost title failed {:?}",e),
+            panic!("failed to parse header garbage \n{:?}",e),
     };
-    let _body = title.0;
-    let mut header = title.1.to_string();
-    let header = enclose(header.as_mut_str(),"title");
-    
-    println!("{:?}", header );
+
+    let mut header = match parse_title(header_garbage.0) {
+        Ok(i) => i,
+        Err(e) =>
+            panic!("parse blogpost title failed \n{:?}",e),
+    };
+    let mut _title = header.1; // saved for when we need to put it into the file at the end.
+    let mut body = match strip_suffix(header.0) {
+        Ok(i) => i.1,
+        Err(e) =>
+            panic!("failed to strip prefix from body. \n{:?}",e),
+    };
+
+    println!("\n\n\n");
+    println!("{:?}", body);
+
 }
 
 fn parse_title(input: &str) -> IResult<&str, &str> {
-    delimited(tag("<h1>"), is_not("<"), tag("</h"))(input)
+    delimited(tag("<h2>"), is_not("<"), tag("</h2>"))(input)
+}
+    // TODO: need to implement a combinator to search for any h tag not just h2. priority: hi;
+fn strip_till_title(input: &str) -> IResult<&str, &str>{
+    take_until("<h2>")(input) 
+}
+fn strip_prefix(input: &str) -> IResult<&str, &str>{
+    take_until("<p>")(input)
+}
+fn strip_suffix(input: &str) -> IResult<&str, &str>{
+    take_until("HTML;")(input)
+}
+fn parse_body(input: &str) -> IResult<&str, &str> {
+    delimited(tag("<<<HTML"), is_not("1>"), tag("</h"))(input)
 }
 
 fn enclose(content: &str, tag_type: &str) -> String {
@@ -77,8 +104,20 @@ fn body_string() {
     }
 #[test]
 fn title_parse() {
-    let input_str = "<h1>header</h1>";
+    let input_str = "<h2>header</h2>";
     let parse_title = parse_title(input_str);
-    assert_eq!(parse_title, Ok(("1>","header")));
+    assert_eq!(parse_title, Ok(("","header")));
+}
+#[test]
+fn strip_pre() {
+    let input_str = "asdfasdf<p>f";
+    let parse_pre = strip_prefix(input_str);
+    assert_eq!(parse_pre, Ok(("<p>f","asdfasdf")));
+}
+#[test]
+fn strip_suff() {
+    let input_str = "asdfasdfHTML;f";
+    let stripped = strip_suffix(input_str);
+    assert_eq!(stripped, Ok(("HTML;f","asdfasdf")));
 }
 }
