097034bbd90371426f8ad7ad5e1216fdb988190f
Jo Phillips <joxphillips@gmail.com>
2020-09-25 18:27:55 -0600

    Remove Debug Prints. Fix borrows for input_file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod config;
 mod input;
+mod output;
 use clap::App;
 use config::ConfigData;
 use std::fs;
@@ -14,17 +15,23 @@ fn main() {
     .arg("-i --input=[file] 'blog post html file'")
     .arg("-o --output=[file] 'sets the file to put blog post xml data if one is not supplied a file will be created")
     .arg("-c --config=[file] 'sets a config file to use for the url and other information that can't be gleemed from the input file.'")
+    .arg("-d --description 'gives the description of the blogpost that is being read in by -i'")
     .arg("-n --new_conf 'prints default config in your systems config directory. default values will not work must change before program can be run.'")
     .get_matches();
 
-    let mut file_name = String::new();
+    let mut input_file = String::new();
+    let mut output_file = String::from("default");
     //println!("{:?}",matches.value_of("input"));
     match matches.value_of("input") {
-        Some(x) => file_name = x.to_string(),
-        None => println!("Please enter a file to parse"),
+        Some(x) => input_file = x.to_string(),
+        None => println!("make sure you include an html or php file with the --i or --input option"),
+    }
+    match matches.value_of("output") {
+        Some(x) => output_file = x.to_string(),
+        None => {println!("couldn't find output file creating new file in current directory...");},
     }
     let mut contents =
-        fs::read_to_string(file_name).expect("Something went wrong opening the file.");
+        fs::read_to_string(&input_file).expect("Something went wrong opening the file.");
     let header_garbage = match strip_till_title(contents.as_mut_str()) {
         // should return tuple of .0=header_garbagef.1 rest of the contents of the file.
         Ok(i) => i,
@@ -36,20 +43,19 @@ fn main() {
         Ok(i) => i,
         Err(e) => panic!("parse blogpost title failed \n{:?}", e),
     };
-    let mut _title = header.1; // saved for when we need to put it into the file at the end.
-    let mut body = match strip_suffix(header.0) {
+    let title = header.1; // saved for when we need to put it into the file at the end.
+    let body = match strip_suffix(header.0) {
         Ok(i) => i.1,
         Err(e) => panic!("failed to strip prefix from body. \n{:?}", e),
     };
+
     let body = &body.replace("\n", " ").to_string();
-    println!("\n\n\n");
-    println!("{:?}", body);
-    let conf = config::ConfigData::new(
-        "dreamrot",
-        "https://dreamrot.com/blog",
-        "a blog about coding art and life",
-        "/posts",
-        "en-us",
-    );
+    let mut conf = config::read_config();
+
+    //println!("\n\n\n");
+    //println!("{:?}", body);
+    
+   let post = config::Post::new(title, conf.language.as_mut_str(), body, "", &input_file);
+   output::write_to_out_file(post, output_file.as_mut_str(), conf);
 }
 
