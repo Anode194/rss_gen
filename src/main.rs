a51d2f14ca4242eb1def4d602661272605c815ce
Jo Phillips <joxphillips@gmail.com>
2020-11-28 14:59:58 -0700

    Main Refactor To Use Path

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ mod input;
 mod output;
 use clap::App;
 use config::ConfigData;
-use std::fs;
 use input::*;
+use std::fs;
+use std::path::Path;
 extern crate nom;
 
 fn main() {
@@ -24,38 +25,46 @@ fn main() {
     //println!("{:?}",matches.value_of("input"));
     match matches.value_of("input") {
         Some(x) => input_file = x.to_string(),
-        None => println!("make sure you include an html or php file with the --i or --input option"),
+        None => {
+            println!("make sure you include an html or php file with the --i or --input option")
+        }
     }
     match matches.value_of("output") {
         Some(x) => output_file = x.to_string(),
-        None => {println!("couldn't find output file creating new file in current directory...");},
+        None => {
+            println!("couldn't find output file creating new file in current directory...");
+        }
     }
-    let mut contents =
+    let path = Path::new(&input_file);
+    if path.is_dir() {
+        let posts = post_dir(&path);
+    } else {
+        let mut contents =
         fs::read_to_string(&input_file).expect("Something went wrong opening the file.");
-    let header_garbage = match strip_till_title(contents.as_mut_str()) {
-        // should return tuple of .0=header_garbagef.1 rest of the contents of the file.
-        Ok(i) => i,
+        let header_garbage = match strip_till_title(contents.as_mut_str()) {
+            // should return tuple of .0=header_garbagef.1 rest of the contents of the file.
+            Ok(i) => i,
 
-        Err(e) => panic!("failed to parse header garbage \n{:?}", e),
-    };
+            Err(e) => panic!("failed to parse header garbage \n{:?}", e),
+        };
 
-    let header = match parse_title(header_garbage.0) {
-        Ok(i) => i,
-        Err(e) => panic!("parse blogpost title failed \n{:?}", e),
-    };
-    let title = header.1; // saved for when we need to put it into the file at the end.
-    let body = match strip_suffix(header.0) {
-        Ok(i) => i.1,
-        Err(e) => panic!("failed to strip prefix from body. \n{:?}", e),
-    };
+        let header = match parse_title(header_garbage.0) {
+            Ok(i) => i,
+            Err(e) => panic!("parse blogpost title failed \n{:?}", e),
+        };
+        let title = header.1; // saved for when we need to put it into the file at the end.
+        let body = match strip_suffix(header.0) {
+            Ok(i) => i.1,
+            Err(e) => panic!("failed to strip prefix from body. \n{:?}", e),
+        };
 
-    let body = &body.replace("\n", " ").to_string();
-    let mut conf = config::read_config();
+        let body = &body.replace("\n", " ").to_string();
+        let mut conf = config::read_config();
 
-    //println!("\n\n\n");
-    //println!("{:?}", body);
-    
-   let post = config::Post::new(title, conf.language.as_mut_str(), body, "", &input_file);
-   output::write_to_out_file(post, output_file.as_mut_str(), conf);
+        //println!("\n\n\n");
+        //println!("{:?}", body);
+
+        let post = config::Post::new(title, conf.language.as_mut_str(), body, "", &input_file);
+        output::write_to_out_file(post, output_file.as_mut_str(), conf);
+    }
 }
-
